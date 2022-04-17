214407643434efeae20af60d44cdd58c3ffac062
paul <paulbrowne2@gmail.com>
2022-04-17 12:50:09 +0100

    Added block content to templates, simple home and inbox views

--- inbox/app/views.py
+++ inbox/app/views.py
@@ -1,3 +1,9 @@
+from tkinter.messagebox import RETRY
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def inbox(request):
+    return render(request, "inbox.html")
